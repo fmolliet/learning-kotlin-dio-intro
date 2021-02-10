6dca8f1aece3d8249a7ee36126317a6b9e9fe1ea
fmolliet <fmolliet@hotmail.com>
2021-02-10 14:43:08 -0300

    Adicionando declarações de variaveis

--- src/main.kt
+++ src/main.kt
@@ -11,15 +11,22 @@ Var - Mutavel, CamelCase
 Val - Imutável, CamelCase
 Const Val - Valor Imutavel, SNAKE_UPPERCASE - constante
 */
+package app
 
 const val MIN_AGE = 16
 const val MAX_AGE = 90
 
 fun main(args: Array<String>){
+    // Variavel com tipagem para string
     var currentAge:Int
     
     println(MAX_AGE)
     
     currentAge = 22
     println(currentAge)
+    // Você não pode alterar o tipo da variavel,
+    // precisa ser convertido
+    currentAge = "200".toInt()
+    println(currentAge)
+
 }
